4420938db37b77a87cd4615814061d2a8c77f3e2
zhikuanaw <xiaoichiban@hotmail.com>
2021-03-14 22:17:08 +0800

    modify profile edit and registration page on password requirement for better UX

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  validates :email, :name, :contact_number, :password, :password_confirmation, presence: true
+  validates :email, :name, :contact_number, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
